55026f341da3c1fafd38784c09131486e0b82e98
Mark Marilag <mark.marilag27@gmail.com>
2021-10-27 10:50:16 +0800

    [Login UI] implement login with validation.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,91 @@
 <template>
-  <div class="bg-white shadow rounded py-4 px-6">
-    Foo
+  <div
+    id="login-page"
+    class="flex-grow max-w-sm bg-white rounded shadow py-8 px-6"
+  >
+    <h1 class="text-3xl font-bold mb-1">
+      SimplyATS
+    </h1>
+    <!-- end title -->
+    <p class="text-gray-500 mb-4">
+      Sign in to continue
+    </p>
+    <!-- end subtitle -->
+
+    <form @submit.prevent="onSubmit">
+      <BaseInput
+        v-model="form.email"
+        type="email"
+        placeholder="Type in email..."
+        :error="errors.email"
+        @clear="errors.email = undefined"
+      />
+      <!-- end email -->
+      <BaseInput
+        v-model="form.password"
+        type="password"
+        placeholder="Type in password..."
+        :error="errors.password"
+        @clear="errors.password = undefined"
+      />
+      <!-- end password -->
+      <BaseButton
+        type="submit"
+        class="mt-2 bg-black text-white py-4"
+        :disabled="isLoading"
+      >
+        <span class="flex justify-center items-center">
+          <SVGAnimateSpin v-if="isLoading" />
+          <span :class="{ 'ml-3': isLoading }">Login</span>
+        </span>
+      </BaseButton>
+      <!-- end button -->
+    </form>
+    <!-- end form -->
   </div>
-  <!-- end login -->
 </template>
 
 <script>
+import { postLogin } from '@/api/auth'
+import { mapActions } from 'vuex'
+import BaseInput from '@/components/Base/BaseInput.vue'
+import BaseButton from '@/components/Base/BaseButton.vue'
+import SVGAnimateSpin from '@/components/SVG/SVGAnimateSpin.vue'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+
+  components: {
+    BaseInput,
+    BaseButton,
+    SVGAnimateSpin
+  },
+
+  data: () => ({
+    isLoading: false,
+    form: {
+      email: null,
+      password: null
+    },
+    errors: {}
+  }),
+
+  methods: {
+    ...mapActions({
+      setUser: 'auth/setUser'
+    }),
+    onSubmit () {
+      this.errors = {}
+      this.isLoading = true
+
+      postLogin(this.form)
+        .then(({data}) => {
+          this.setUser(data)
+          this.$router.push({ name: 'home' })
+        })
+        .catch((error) => this.errors = error?.response?.data?.errors)
+        .finally(() => this.isLoading = false)
+    }
+  }
 }
 </script>
